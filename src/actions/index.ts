bad8bbf433e5e8ba9fee92dfeead8c19f7f7aabe
Vinh Vu <vinhhv@uci.edu>
2024-05-23 11:56:41 -0700

    Handle errors for snippet creation

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -7,30 +7,39 @@ export async function createSnippet(
   formState: { message: string },
   formData: FormData
 ) {
-  // Check the user's input and validate it
-  const title = formData.get("title");
-  const code = formData.get("code");
+  try {
+    // Check the user's input and validate it
+    const title = formData.get("title");
+    const code = formData.get("code");
 
-  if (typeof title !== "string" || title.length < 3) {
-    return {
-      message: "Title must be longer",
-    };
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer",
+      };
+    }
+
+    if (typeof code !== "string" || code.length < 10) {
+      return {
+        message: "Code must be longer",
+      };
+    }
+
+    // Create a new record in the database
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      message: error.message;
+    } else {
+      return {
+        message: "Something went wrong...",
+      };
+    }
   }
-
-  if (typeof code !== "string" || code.length < 10) {
-    return {
-      message: "Code must be longer",
-    };
-  }
-
-  // Create a new record in the database
-  await db.snippet.create({
-    data: {
-      title,
-      code,
-    },
-  });
-
   // Redirect the user back to the root route
   redirect("/");
 }
