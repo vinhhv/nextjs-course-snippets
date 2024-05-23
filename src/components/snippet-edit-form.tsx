dadcbff34549e1abbf6b4b73c448fe34d5e9629c
Vinh Vu <vinhhv@uci.edu>
2024-05-23 10:54:30 -0700

    Set up state for Editor component

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -2,12 +2,19 @@
 
 import Editor from "@monaco-editor/react";
 import { Snippet } from "@prisma/client";
+import { useState } from "react";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+  const [code, setCode] = useState(snippet.code);
+
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
+
   return (
     <div>
       <Editor
@@ -18,6 +25,7 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         options={{
           minimap: { enabled: false },
         }}
+        onChange={handleEditorChange}
       />
     </div>
   );
